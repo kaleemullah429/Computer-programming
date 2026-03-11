53f43b5284d8e7f4c6298bed0bc6264a3677fbf1
Kaleem Ullah <kaleemiamkhan@gmail.com>
2026-03-11 11:00:12 +0500

    Add files via upload

---
+++ Lab 2/Lab_Tasks/Display_Pi_Value.cpp
@@ -0,0 +1,23 @@
+                                // Lab 2 Task 11.cpp
+                                // Name: [Kaleem Ullah]
+                                // Registration Number: [BF25NWELE0661]
+
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    // Program for declaring and Display of Floating Point Variable "Pi"
+
+    //Float data type
+    float pi = 3.14;
+    
+    //Basic output for value of Pi
+    cout << "The value of Pi is: " << pi << endl;
+    
+    return 0;
+}
+
+
