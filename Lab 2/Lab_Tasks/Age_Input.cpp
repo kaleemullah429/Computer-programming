724f73e12be23a910cc74281f268c3f613afcdfd
Kaleem Ullah <kaleemiamkhan@gmail.com>
2026-03-11 10:59:44 +0500

    Add files via upload

---
+++ Lab 2/Lab_Tasks/Age_Input.cpp
@@ -0,0 +1,23 @@
+                                // Lab 2 Task 4.cpp
+                                // Name: [Kaleem Ullah]
+                                // Registration Number: [BF25NWELE0661]
+
+
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    // Variable declaration
+    int a;
+
+    //Basic output
+    cout << "Enter Your Age;" << endl;
+
+    //Basic input
+    cin >> a;
+    
+    return 0;
+}
+
---
+++ Lab 2/Lab_Tasks/Print_Name_10_Times.cpp
@@ -0,0 +1,29 @@
+							   // Lab 2 Task 5.cpp
+							   // Name: [Kaleem Ullah]
+							   // Registration Number: [BF25NWELE0661]
+
+
+
+//print your name 10 times 
+
+#include <iostream>
+using namespace std;
+
+int main()
+	{
+	
+    cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;
+	cout<<" Zawar Shah "<<endl;	
+		
+	return 0;
+}
+
+
