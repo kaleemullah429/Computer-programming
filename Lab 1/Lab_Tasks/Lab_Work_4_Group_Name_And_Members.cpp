e12bda1305a0809a93f06ba0011aa886b7b76848
Kaleem Ullah <kaleemiamkhan@gmail.com>
2026-03-11 10:57:38 +0500

    Add files via upload

---
+++ Lab 1/Lab_Tasks/Lab_Work_4_Group_Name_And_Members.cpp
@@ -0,0 +1,14 @@
+                                    // Lab 1.4 - Group Name
+                                    // Name: [Kaleem Ullah]
+                                    // Registration Number: [BF25NWELE0661]
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Output the group name and members
+    cout << "Our Group Name is: The Innovators" << endl;
+    cout << "Our Group Members are:" << endl;
+    cout << "1. Zawar Shah " << endl;
+    cout << "2. Muhammad Adil " << endl;
+    cout << "3. Shehzad Gul Khattak " << endl;
+}
